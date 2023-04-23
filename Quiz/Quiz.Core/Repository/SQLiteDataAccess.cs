860f907cc292b03f5ed53f2b715a418ebf62af11
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-04-23 05:28:54 +0200

    Added a class to communicate with the database

---
+++ Quiz/Quiz.Core/Repository/SQLiteDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using Quiz.Core.ViewModels;
+using System.Configuration;
+using System.Data.SQLite;
+
+namespace Quiz.Core.Repository
+{
+    public class SQLiteDataAccess
+    {
+        public static void CreateQuizz(CreateViewModel model)
+        {
+            SQLiteConnection conn = new SQLiteConnection(LoadConnectionString());
+            conn.Open();
+
+            string sql = "INSERT INTO Quizzes (Name) VALUES (@Name)";
+
+            SQLiteCommand command = new SQLiteCommand(sql, conn);
+
+            command.Parameters.AddWithValue("@Name", model.Name);
+
+            command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        private static string LoadConnectionString(string id = "Default" )
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
--- Quiz/Quiz.Core/ViewModels/CreateViewModel.cs
+++ Quiz/Quiz.Core/ViewModels/CreateViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using Quiz.Core.Core;
+using Quiz.Core.Repository;
 using Quiz.Core.Services;
 using Quiz.Core.UserControls.ViewModels;
 using System.Collections.ObjectModel;
 
 namespace Quiz.Core.ViewModels
 {
-    public class CreateViewModel :ViewModel
+    public class CreateViewModel : ViewModel
     {
         //Properties
         private INavigationService _navigation;
@@ -21,14 +22,21 @@ namespace Quiz.Core.ViewModels
         public RelayCommand NavigateToMainViewCommand { get; set; }
         public RelayCommand AddNewQuestionCommand { get; set; } 
         public ObservableCollection<NewQuestionViewModel> NewQuestionList { get; set; } = new ObservableCollection<NewQuestionViewModel>();
+        public string Name { get; set; } = "Title";
 
         //Constructor
         public CreateViewModel(INavigationService navigation)
         {
             Navigation = navigation;
+
+            var newQuestion = new NewQuestionViewModel
+            {
+                QuestionNumber = NewQuestionList.Count + 1,
+            };
+            NewQuestionList.Add(newQuestion);
+
             NavigateToMainViewCommand = new RelayCommand(o => { Navigation.NavigateTo<MainViewModel>();  }, o => true);
             AddNewQuestionCommand = new RelayCommand(o => AddNewQuestion(), o => true);
-            AddNewQuestion();
         }
         
         //Methods
@@ -39,6 +47,7 @@ namespace Quiz.Core.ViewModels
                 QuestionNumber = NewQuestionList.Count + 1,
             };
             NewQuestionList.Add(newQuestion);
+            SQLiteDataAccess.CreateQuizz(this);
         }
     }
 }
