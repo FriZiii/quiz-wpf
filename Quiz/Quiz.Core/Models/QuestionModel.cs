3b0160c54880c3729908ea92a6c10711c5a4db2f
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-04-26 00:42:02 +0200

    Create QuestionModel.cs

---
+++ Quiz/Quiz.Core/Models/QuestionModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Quiz.Core.Models
+{
+    public class QuestionModel
+    {
+        public int ID { get; set; }
+        public string Question { get; set; }
+
+        public List<AnswerModel> Answers { get; set; }
+    }
+}
