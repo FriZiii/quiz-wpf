cf244eb695700c155c30bd196656f951517b3751
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-04-27 17:43:26 +0200

    Added display of questions in edit box

---
+++ Quiz/Quiz.Core/UserControls/ViewModels/SingleQuestionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Quiz.Core.Core;
+using Quiz.Core.Models;
+using System;
+
+namespace Quiz.Core.UserControls.ViewModels
+{
+    public class SingleQuestionViewModel : ViewModel
+    {
+        //Properties
+        public RelayCommand GetQuestionCommand { get; set; }
+        public QuestionModel QuestionModel { get; set; }
+
+        public static Action<QuestionModel> ShowQuestionEvent;
+        //Constructor
+        public SingleQuestionViewModel(QuestionModel model)
+        {
+            QuestionModel = model;
+            GetQuestionCommand = new RelayCommand(o => { ShowQuestionEvent?.Invoke(QuestionModel); }, o => true);
+        }
+
+        //Methods
+        public void PrintQuestion()
+        {
+            Console.WriteLine(QuestionModel.Question);
+            foreach(var answer in QuestionModel.Answers)
+                Console.WriteLine(answer.Answer);
+        }
+    }
+}
